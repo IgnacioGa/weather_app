8f9b3dcfce67872eef5e8b8c9f4438986f0376f1
Ignacio Gallo <ignacioga.346@gmail.com>
2023-05-31 16:06:07 +0300

    change static url

--- config/settings.py
+++ config/settings.py
@@ -138,8 +138,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "media"), os.path.join(BASE_DIR, "theme")]
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "theme")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Media Files
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
